31c642baf03175ac89f80e59bfe143c391c88fca
Ozzors <oswaldogp16@gmail.com>
2025-09-02 16:54:09 -0400

    Update test_logo.py

--- test_logo.py
+++ test_logo.py
@@ -1,9 +1,1 @@
-import streamlit as st
 
-# Prueba de logo
-st.title("Prueba de Logo")
-st.image("assets/logo.png", width=200)
-
-with st.sidebar:
-    st.image("assets/logo.png", width=150)
-    st.write("Logo en la barra lateral")
